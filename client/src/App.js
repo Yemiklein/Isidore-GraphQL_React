7535887aaae2033f557d1d6d8cb40f0bd2da931c
Adeyemi <yemiklein77@gmail.com>
2022-11-17 04:34:32 +0100

    updated css

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Bookings from "./pages/Bookings";
 import Events from "./pages/Events";
 import MainNavigation from "./components/Navigation/MainNavigation";
+import AuthContext from './context/auth-context'
 
 
-function App() {
+class App extends Component {
+  state = {
+    token: null,
+    userId: null
+  };
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  };
+
+  logout = () => {
+    this.setState({ token: null, userId: null });
+  };
+  render() {
   return (
 <BrowserRouter>
 <React.Fragment>
+<AuthContext.Provider
+            value={{
+              token: this.state.token,
+              userId: this.state.userId,
+              login: this.login,
+              logout: this.logout
+            }}
+          >
 <MainNavigation />
 <main className="main-content">
 <Routes>
-<Route path="/" element={<Navigate to='/auth' exact />}/>
-<Route path="/auth" element={<Auth />} />
+{!this.state.token && <Route path="/" element={<Navigate to='/auth' exact />}/>}
+{this.state.token && <Route path="/" element={<Navigate to='/events' exact />}/>}
+{this.state.token && <Route path="/auth" element={<Navigate to='/events' exact />}/>}
+{!this.state.token && ( <Route path="/auth" element={<Auth />} /> )}
  <Route path="/events" element={<Events />} />
-  <Route path="/bookings" element={<Bookings />} />        
+ {this.state.token && (<Route path="/bookings" element={<Bookings />} /> )}      
  </Routes>
  </main>
+ </AuthContext.Provider>
  </React.Fragment>
 </BrowserRouter>
   );
 }
-
+}
 export default App;

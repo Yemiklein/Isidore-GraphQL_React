c9b562b5501914e3d2c9ee0170990585d06d7c6e
Adeyemi <yemiklein77@gmail.com>
2022-11-17 02:45:37 +0100

    context api

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,23 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Bookings from "./pages/Bookings";
 import Events from "./pages/Events";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 
 function App() {
   return (
 <BrowserRouter>
+<React.Fragment>
+<MainNavigation />
+<main className="main-content">
 <Routes>
 <Route path="/" element={<Navigate to='/auth' exact />}/>
 <Route path="/auth" element={<Auth />} />
  <Route path="/events" element={<Events />} />
   <Route path="/bookings" element={<Bookings />} />        
  </Routes>
+ </main>
+ </React.Fragment>
 </BrowserRouter>
   );
 }

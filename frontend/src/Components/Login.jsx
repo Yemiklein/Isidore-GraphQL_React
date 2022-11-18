119bda506f8f85e3aa158c526140b5d402fb888b
Adeyemi <yemiklein77@gmail.com>
2022-11-18 11:44:23 +0100

    new client setup

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import './Login.css'
 
 
-export const Login = () => {
+export const Login = () =>{
+    const [email, setEmail] = useState("");
     return (
-        <div>
-        <h1>Login</h1>
+        <div className="auth-component">
+            <div className="auth-heading">
+               <h2>Log In</h2> : <h2>Register</h2>
+            </div>
+            {/* {error !== '' && <div className="error">{error}</div> } */}
+            <form className="authform-container">
+                <div className="auth-field">
+                    <label>Enter email: </label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+                </div>
+
+                <div className="auth-field">
+                    <label>Enter password: </label>
+                    <input  placeholder="Password"></input>
+                </div>
+
+                <button className="auth-btn">
+                    {'login' ? 'Log In' : 'Register'}
+                </button>
+            </form>
+            <div className="help-flex">
+                <Link to={`${ 'login' ? '/register' : '/login'}`}>
+                    { 'login' ? 'Register' : 'Log In'}
+                </Link>
+          
+            </div>
         </div>
     )
-    }
+}
--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import {Login} from '../Components/Login'
 export const LoginPage = () => {
     return (
       <div>
-      <h1>Login Page</h1>
+     <Login />
     </div>
     )
   }
